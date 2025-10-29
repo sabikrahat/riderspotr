35ec53e920cfcaa7291d9ce5e97c7c41683eb034
Md. Sabik Alam Rahat <sabikrahat72428@gmail.com>
2025-10-29 16:20:04 +0600

    Refactor PaymentScreen to use ConsumerStatefulWidget; integrate loading state and offerings retrieval with Purchases API

--- lib/presentation/pages/payment/payment_screen.dart
+++ lib/presentation/pages/payment/payment_screen.dart
@@ -1,14 +1,89 @@
+// import 'package:flutter/material.dart';
+// import '../../../core/extensions.dart';
+
+// import '../../widgets/shared/back.dart';
+// import '../../widgets/shared/carbon_background.dart';
+// import '../../widgets/shared/page_padding.dart';
+
+// class PaymentScreen extends StatelessWidget {
+//   const PaymentScreen({super.key});
+
+//   static const String routeName = '/payment';
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       extendBodyBehindAppBar: true,
+//       appBar: AppBar(
+//         backgroundColor: Colors.transparent,
+//         elevation: 0,
+//         leading: Back(),
+//         title: Text('PAYMENT', style: context.textTheme.headlineMedium),
+//         centerTitle: true,
+//       ),
+//       body: CarbonBackground(
+//         imgPath: 'assets/carbon/leaderboard-bg.jpg',
+//         child: PagePadding(
+//           child: Column(
+//             children: [
+//               // Add your payment screen content here
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import '../../../core/extensions.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:purchases_flutter/purchases_flutter.dart';
+import 'package:purchases_ui_flutter/purchases_ui_flutter.dart';
+import 'package:ridespotr/core/extensions.dart';
 
+import '../../../core/toastification.dart';
 import '../../widgets/shared/back.dart';
-import '../../widgets/shared/carbon_background.dart';
-import '../../widgets/shared/page_padding.dart';
+import '../../widgets/shared/loading_overlay.dart';
 
-class PaymentScreen extends StatelessWidget {
+class PaymentScreen extends ConsumerStatefulWidget {
+  static const routeName = '/payment';
   const PaymentScreen({super.key});
 
-  static const String routeName = '/payment';
+  @override
+  ConsumerState<PaymentScreen> createState() => _PaymentScreenState();
+}
+
+class _PaymentScreenState extends ConsumerState<PaymentScreen> {
+  bool _isLoading = false;
+  late Future<Offerings> _getOfferings;
+  late void Function(CustomerInfo) _customerInfoUpdateListener;
+
+  @override
+  void initState() {
+    super.initState();
+    _getOfferings = Purchases.getOfferings();
+    _customerInfoUpdateListener = (info) {
+      setState(() {
+        _isLoading = true;
+      });
+      if (info.activeSubscriptions.isNotEmpty) {
+        if (mounted) {
+          context.go('/');
+        }
+      }
+      setState(() {
+        _isLoading = false;
+      });
+    };
+    Purchases.addCustomerInfoUpdateListener(_customerInfoUpdateListener);
+  }
+
+  @override
+  void dispose() {
+    Purchases.removeCustomerInfoUpdateListener(_customerInfoUpdateListener);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,14 +96,32 @@ class PaymentScreen extends StatelessWidget {
         title: Text('PAYMENT', style: context.textTheme.headlineMedium),
         centerTitle: true,
       ),
-      body: CarbonBackground(
-        imgPath: 'assets/carbon/leaderboard-bg.jpg',
-        child: PagePadding(
-          child: Column(
-            children: [
-              // Add your payment screen content here
-            ],
-          ),
+      body: LoadingOverlay(
+        isLoading: _isLoading,
+        child: FutureBuilder(
+          future: _getOfferings,
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+                return Center(child: CircularProgressIndicator());
+              case ConnectionState.done:
+                if (snapshot.hasError) {
+                  return Center(child: Text(snapshot.error.toString()));
+                } else {
+                  final offerings = snapshot.data!.all;
+                  final offering = offerings[offerings.keys.first];
+                  return PaywallView(
+                    offering: offering,
+                    displayCloseButton: false,
+                    onPurchaseError: (error) {
+                      showErrorMessage(error.message);
+                    },
+                  );
+                }
+              default:
+                return SizedBox();
+            }
+          },
         ),
       ),
     );
